bc689b3cb028a71069e26edfb99c1c599f2ec4df
Daniel Adegoke <danyadegoke@gmail.com>
2021-11-10 14:53:37 +0100

    Fix: Migration issues

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { join } = require('path');
+
 module.exports = {
   type: 'postgres',
   host: process.env.DB_HOST || 'ec2-3-227-181-85.compute-1.amazonaws.com',
@@ -6,12 +9,12 @@ module.exports = {
   password: process.env.DB_PASSWORD || 'cf2b9801dac2b8d505464fcbbf0a1bc88b5b7c67bc28ec60c0d73387e05cf36e',
   database: process.env.DB_NAME || 'd2t8s8cbenc8ou',
   logging: false,
-  entities: ['build/database/models/**/*.js'],
-  migrations: ['build/database/migration/**/*.js'],
-  subscribers: ['build/database/subscriber/**/*.js'],
+  entities: [join(__dirname, 'build/database/models/**/*.js')],
+  migrations: [join(__dirname, 'build/database/migrations/**/*.js')],
+  subscribers: [join(__dirname, 'build/database/subscriber/**/*.js')],
   cli: {
     entitiesDir: 'src/database/models',
-    migrationsDir: 'src/database/migration',
+    migrationsDir: 'src/database/migrations',
     subscribersDir: 'src/database/subscriber',
   },
   extra: {
